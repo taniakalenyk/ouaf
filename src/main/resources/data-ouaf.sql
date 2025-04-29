f317600d134bf379b9f0d02afad8bd52e29d111d
Tetiana <taniakalenyk@gmail.com>
2025-04-29 11:03:22 +0200

    creation of schema and data files

---
+++ src/main/resources/data-ouaf.sql
@@ -0,0 +1,19 @@
+INSERT INTO quote_of_the_day (id, quote)
+VALUES
+    (1, 'Un chien qui aboie est un ami qui s\'exprime.'),
+    (2, 'Les jouets mâchouillés sont des trophées d\'amour.'),
+    (3, 'Un chien qui ronfle est une berceuse pour l\'âme.'),
+    (4, 'Les promenades avec un chien, c\'est du sport... et du ramassage de crottes.'),
+    (5, 'Avoir un chien, c\'est accepter de ne plus jamais manger seul.'),
+    (6, 'Les léchouilles baveuses sont des baisers sincères.'),
+    (7, 'Le chien comprend “promenade” en 0.2 secondes, mais ignore “non” depuis 3 ans.'),
+    (8, 'Un chien mouillé, c’est 90% d’eau et 10% de regrets.'),
+    (9, 'Les empreintes de pattes sur le sol sont des traces de bonheur.'),
+    (10, 'Les chiens savent qu’une maison propre est un concept, pas une réalité.'),
+    (11, 'Un chien peut dormir n’importe où… sauf dans son panier, bien sûr.'),
+    (12, 'Les câlins canins sont les meilleurs remèdes contre le stress.'),
+    (13, 'Un chien qui rapporte la balle est un partenaire de jeu infatigable.'),
+    (14, 'Un chien propre, c’est un concept théorique entre deux flaques.'),
+    (15, 'Un foyer sans chien est une maison sans cœur.'),
+    (16, 'La boue sur les pattes est la preuve d\'une aventure réussie.'),
+    (17, 'Le chien entend l’ouverture du frigo à travers trois murs et un rêve.');
---
+++ src/main/resources/schema-ouaf.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS quote_of_the_day (
+                                                id INT PRIMARY KEY,
+                                                quote TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS quote_of_the_day (
+                                                id INT PRIMARY KEY,
+                                                quote TEXT NOT NULL
+);
