96f4046e4bd3dadbbe5c5d8498bb9e4430776cbf
Tetiana <taniakalenyk@gmail.com>
2025-04-29 13:34:47 +0200

    CreatedDate changed

--- src/main/java/academy/ouaf/model/User.java
+++ src/main/java/academy/ouaf/model/User.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
